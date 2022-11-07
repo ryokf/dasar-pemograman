7a88baa79057460e64dc3fb380b27a11357d24c9
Ryo Khrisna Fitriawan <94428496+ryokf@users.noreply.github.com>
2022-11-07 07:31:56 +0700

    menambah tugas 8 perulangan

---
+++ tugas8/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // for(int i = 0; i <= 100; i += 2){
+    //     cout << i << endl;
+    // }
+
+    // for(int i = 10; i >= 2; i -= 2){
+    //     cout << i << endl;
+    // }
+
+    for(int i = 1; i <= 40; i += 3){
+        cout << i << endl;
+    }
+    return 0;
+}
